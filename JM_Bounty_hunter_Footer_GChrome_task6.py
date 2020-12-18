0aaef900bb14139d38d79abcb40b74be1b6901bf
vickytorivicky <72368267+vickytorivicky@users.noreply.github.com>
2020-12-18 15:33:45 -0500

    JM_Bounty_Hunter_Footer_GChrome_task6

---
+++ JM_Bounty_hunter_Footer_GChrome_task6.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+# Verify Footer's text: "EMPLOYER RESOURCES"
+driver.get("http://jobmight.com/bounty-hunter/stripe-dashboard")
+emp_resources = driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/header/div/div/div/div[2]/div/h3")
+print(emp_resources.text)
+
+driver.quit()
