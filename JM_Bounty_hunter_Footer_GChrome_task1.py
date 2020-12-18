5350cd0708d7e4b6c2ffdc1c0eecf434298483c2
vickytorivicky <72368267+vickytorivicky@users.noreply.github.com>
2020-12-18 15:21:44 -0500

    JM_Bounty_Hunter_Footer_GChrome_task1

---
+++ JM_Bounty_hunter_Footer_GChrome_task1.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.get("http://jobmight.com/dashboard")
+def verifyLogo():
+# Wait until logo on the footer is visible and verify its text
+   logo = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@style,'display: block; justify-content: left; margin: 0px auto;')]")))
+   assert logo.text == "JobsMight"
+
+driver.quit()
